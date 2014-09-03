57dda6e406ea54e6da4c105ed885cc1670f00296
Austin Cherry <chillingwithim@gmail.com>
2014-09-03 09:31:41 -0700

    code for this week article

--- crypto/main.go
+++ crypto/main.go
@@ -6,33 +6,68 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"time"
 )
 
+// Contains our sql connection.
 type DBHandler struct {
 	db *sql.DB
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root@/mobile_lsfilter_dev")
+	// Open SQL connection to db.
+	db, err := sql.Open("mysql", "root@/tester")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	// Pass it to the handler.
 	h := DBHandler{db: db}
 
-	http.HandleFunc("/", h.HomeHandler)
+	// A couple HTTP routes.
+	http.HandleFunc("/create", h.CreateHandler)
+	http.HandleFunc("/auth", h.AuthHandler)
 	http.ListenAndServe(":8081", nil)
 }
 
-func (h *DBHandler) HomeHandler(rw http.ResponseWriter, req *http.Request) {
-	name := "dalton"
-	password := []byte("test")
+func (h *DBHandler) CreateHandler(rw http.ResponseWriter, req *http.Request) {
+	// Get the form values out of the POST request.
+	name := req.FormValue("name")
+	password := req.FormValue("password")
+
+	// Generate a hashed password from bcrypt.
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Stick that in our users table of our db.
+	_, err = h.db.Query("INSERT INTO users (name, password_digest, created_at, updated_at) VALUES(?,?,?,?)",
+		name, hashedPass, time.Now(), time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write a silly message back to the client.
+	rw.Write([]byte("Created user!"))
+}
+
+func (h *DBHandler) AuthHandler(rw http.ResponseWriter, req *http.Request) {
+	// Get the form values out of the POST request.
+	name := req.FormValue("name")
+	password := req.FormValue("password")
+
+	// Find the user by his name and get the password_digest we generated in the create method out.
 	var digest string
 	if err := h.db.QueryRow("SELECT password_digest FROM users WHERE name = ?", name).Scan(&digest); err != nil {
 		log.Fatal(err)
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(digest), password); err != nil {
+	// Compare that password_digest to our password we got from the form value.
+	// If the error is not equal to nil, we know the auth failed. If there is no error, it
+	// was successful.
+	if err := bcrypt.CompareHashAndPassword([]byte(digest), []byte(password)); err != nil {
 		rw.Write([]byte("auth failure..."))
 	} else {
 		rw.Write([]byte("auth successful!"))
